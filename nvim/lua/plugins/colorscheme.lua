ca565f4d9f0907047b31c05f470154ba06250a07
Dessemy <Dessmy@proton.me>
2026-08-01 19:49:22 +0700

    Update colorscheme.lua

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,21 @@
 -- lua/plugins/colorscheme.lua
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false, -- load at startup since it's the default theme
-    priority = 1000, -- load before other plugins
+    "folke/tokyonight.nvim",
+    name = "tokyonight",
+    lazy = false, -- default theme, load at startup
+    priority = 1000,
     opts = {
-      flavour = "mocha", -- hardcoded: always mocha, never "auto"
-      transparent_background = false,
-      term_colors = true,
+      style = "night", -- night | storm | day | moon
+      transparent = false,
+      terminal_colors = true,
       styles = {
-        comments = { "italic" },
-        keywords = { "italic" },
+        comments = { italic = true },
+        keywords = { italic = true },
         functions = {},
-        strings = {},
         variables = {},
       },
-      integrations = {
+      plugins = {
         cmp = true,
         gitsigns = true,
         telescope = true,
@@ -25,26 +24,11 @@ return {
         mason = true,
         which_key = true,
         indent_blankline = { enabled = true },
-        native_lsp = {
-          enabled = true,
-          virtual_text = {
-            errors = { "italic" },
-            hints = { "italic" },
-            warnings = { "italic" },
-            information = { "italic" },
-          },
-          underlines = {
-            errors = { "underline" },
-            hints = { "underline" },
-            warnings = { "underline" },
-            information = { "underline" },
-          },
-        },
       },
     },
     config = function(_, opts)
-      require("catppuccin").setup(opts)
-      vim.cmd.colorscheme("catppuccin-mocha")
+      require("tokyonight").setup(opts)
+      vim.cmd.colorscheme("tokyonight-night")
     end,
   },
 }
