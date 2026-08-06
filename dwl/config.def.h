799fd91e6361d2b41d22d980737739612eb1b134
Dessemy <Dessmy@proton.me>
2026-08-06 17:50:34 +0700

    Update config.def.h

--- dwl/config.def.h
+++ dwl/config.def.h
@@ -109,18 +109,19 @@ static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TA
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 static const char *termcmd[]         = { "foot", NULL };
-static const char *qutebrowsercmd[]  = { "qutebrowser", NULL };
-static const char *hyprpickercmd[]   = { "hyprpicker", NULL };
+static const char *qutecmd[]         = { "qutebrowser", NULL };
+static const char *pickrcmd[]        = { "hyprpicker", NULL };
 
 static const Key keys[] = {
-	{ MODKEY,                              XKB_KEY_Tab,            spawn,            SHCMD("foot -e $HOME/.config/scripts/launcher") },
-	{ MODKEY,                              XKB_KEY_Escape,         spawn,            SHCMD("foot -e $HOME/.config/scripts/powermenu") },
-	{ MODKEY|WLR_MODIFIER_SHIFT,           XKB_KEY_W,              spawn,            SHCMD("foot -e $HOME/.config/scripts/walls") },
-	{ MODKEY,                              XKB_KEY_q,              spawn,            {.v = qutebrowsercmd} },
-	{ MODKEY,                              XKB_KEY_p,              spawn,            {.v = hyprpickercmd} },
 	{ MODKEY,                              XKB_KEY_Return,         spawn,            {.v = termcmd} },
-  { MODKEY,                              XKB_KEY_s,              spawn,            SHCMD("$HOME/.config/scripts/snip full") },
-  { MODKEY|WLR_MODIFIER_CTRL,            XKB_KEY_s,              spawn,            SHCMD("$HOME/.config/scripts/snip area") },
+    { MODKEY,                              XKB_KEY_q,              spawn,            {.v = qutecmd} },
+	{ MODKEY,                              XKB_KEY_p,              spawn,            {.v = pickrcmd} },
+    { MODKEY,                              XKB_KEY_s,              spawn,            SHCMD("$HOME/.config/scripts/snip full") },
+    { MODKEY|WLR_MODIFIER_CTRL,            XKB_KEY_s,              spawn,            SHCMD("$HOME/.config/scripts/snip area") },
+	{ MODKEY,                              XKB_KEY_Tab,            spawn,            SHCMD("foot -e $HOME/.config/scripts/launchr") },
+	{ MODKEY,                              XKB_KEY_Escape,         spawn,            SHCMD("foot -e $HOME/.config/scripts/pwermnu") },
+	{ MODKEY|WLR_MODIFIER_SHIFT,           XKB_KEY_W,              spawn,            SHCMD("foot -e $HOME/.config/scripts/walls") },
+    { MODKEY|WLR_MODIFIER_ALT,             XKB_KEY_c,              spawn,            SHCMD("foot -e $HOME/.config/scripts/clipbrd"),
 	{ MODKEY,                              XKB_KEY_y,              spawn,            SHCMD("foot -e yazi") },
 	{ MODKEY,                              XKB_KEY_v,              spawn,            SHCMD("foot -e nvim") },
 	{ MODKEY,                              XKB_KEY_r,              spawn,            SHCMD("foot -e rmpc") },
@@ -130,8 +131,7 @@ static const Key keys[] = {
 	{ MODKEY,                              XKB_KEY_t,              spawn,            SHCMD("foot -e ttyper") },
 	{ MODKEY,                              XKB_KEY_w,              spawn,            SHCMD("foot -e wiremix") },
 	{ MODKEY,                              XKB_KEY_i,              spawn,            SHCMD("foot -e impala") },
-	{ MODKEY|WLR_MODIFIER_ALT,             XKB_KEY_c,              spawn,            SHCMD("foot -e $HOME/.config/scripts/clipmenu") },
-
+	
 	{ 0,                                   XKB_KEY_XF86MonBrightnessUp,   spawn,     SHCMD("brightnessctl set +5%") },
 	{ 0,                                   XKB_KEY_XF86MonBrightnessDown, spawn,     SHCMD("brightnessctl set 5%-") },
 
@@ -157,25 +157,24 @@ static const Key keys[] = {
 	{ MODKEY|WLR_MODIFIER_SHIFT,           XKB_KEY_B,              togglebar,        {0} },
 	{ MODKEY,                              XKB_KEY_Return,         zoom,             {0} },
 
-	{ MODKEY|WLR_MODIFIER_CTRL,            XKB_KEY_c,              killclient,       {0} },
+	{ MODKEY|WLR_MODIFIER_CTRL,            XKB_KEY_c,                killclient,       {0} },
 
-	{ MODKEY,                              XKB_KEY_0,              view,             {.ui = ~0} },
-	{ MODKEY|WLR_MODIFIER_SHIFT,           XKB_KEY_parenright,     tag,              {.ui = ~0} },
-	{ MODKEY,                              XKB_KEY_comma,          focusmon,         {.i = WLR_DIRECTION_LEFT} },
-	{ MODKEY,                              XKB_KEY_period,         focusmon,         {.i = WLR_DIRECTION_RIGHT} },
-	{ MODKEY|WLR_MODIFIER_SHIFT,           XKB_KEY_less,           tagmon,           {.i = WLR_DIRECTION_LEFT} },
-	{ MODKEY|WLR_MODIFIER_SHIFT,           XKB_KEY_greater,        tagmon,           {.i = WLR_DIRECTION_RIGHT} },
-	TAGKEYS(          XKB_KEY_1, XKB_KEY_exclam,                     0),
-	TAGKEYS(          XKB_KEY_2, XKB_KEY_at,                         1),
-	TAGKEYS(          XKB_KEY_3, XKB_KEY_numbersign,                 2),
-	TAGKEYS(          XKB_KEY_4, XKB_KEY_dollar,                     3),
-	TAGKEYS(          XKB_KEY_5, XKB_KEY_percent,                    4),
-	TAGKEYS(          XKB_KEY_6, XKB_KEY_asciicircum,                5),
-	TAGKEYS(          XKB_KEY_7, XKB_KEY_ampersand,                  6),
-	TAGKEYS(          XKB_KEY_8, XKB_KEY_asterisk,                   7),
-	TAGKEYS(          XKB_KEY_9, XKB_KEY_parenleft,                  8),
+	{ MODKEY,                              XKB_KEY_0,                view,             {.ui = ~0} },
+	{ MODKEY|WLR_MODIFIER_SHIFT,           XKB_KEY_parenright,       tag,              {.ui = ~0} },
+	{ MODKEY,                              XKB_KEY_comma,            focusmon,         {.i = WLR_DIRECTION_LEFT} },
+	{ MODKEY,                              XKB_KEY_period,           focusmon,         {.i = WLR_DIRECTION_RIGHT} },
+	{ MODKEY|WLR_MODIFIER_SHIFT,           XKB_KEY_less,             tagmon,           {.i = WLR_DIRECTION_LEFT} },
+	{ MODKEY|WLR_MODIFIER_SHIFT,           XKB_KEY_greater,          tagmon,           {.i = WLR_DIRECTION_RIGHT} },
+	TAGKEYS(                               XKB_KEY_1,                XKB_KEY_exclam,                     0),
+	TAGKEYS(                               XKB_KEY_2,                XKB_KEY_numbersign,                 2),
+	TAGKEYS(                               XKB_KEY_4,                XKB_KEY_dollar,                     3),
+	TAGKEYS(                               XKB_KEY_5,                XKB_KEY_percent,                    4),
+	TAGKEYS(                               XKB_KEY_6,                XKB_KEY_asciicircum,                5),
+	TAGKEYS(                               XKB_KEY_7,                XKB_KEY_ampersand,                  6),
+	TAGKEYS(                               XKB_KEY_8,                XKB_KEY_asterisk,                   7),
+	TAGKEYS(                               XKB_KEY_9,                XKB_KEY_parenleft,                  8),
 
-	{ MODKEY|WLR_MODIFIER_SHIFT,           XKB_KEY_Q,              quit,             {0} },
+	{ MODKEY|WLR_MODIFIER_SHIFT,           XKB_KEY_Q,                quit,           {0} },
 	{ WLR_MODIFIER_CTRL|WLR_MODIFIER_ALT,  XKB_KEY_Terminate_Server, quit,           {0} },
 
 #define CHVT(n) { WLR_MODIFIER_CTRL|WLR_MODIFIER_ALT,XKB_KEY_XF86Switch_VT_##n, chvt, {.ui = (n)} }
