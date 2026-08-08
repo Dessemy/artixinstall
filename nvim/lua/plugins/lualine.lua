b9109ec00bdfaaef116a74c9dee39751299519db
Dessemy <Dessmy@proton.me>
2026-08-08 23:19:39 +0700

    Update lualine.lua

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,24 +1,106 @@
--- lua/plugins/lualine.lua
 return {
   {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    build = ":MasonUpdate",
     opts = {
-      options = {
-        theme = "catppuccin",
-        globalstatus = true,
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
-      },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = { { "filename", path = 1 } },
-        lualine_x = { "encoding", "fileformat", "filetype" },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
-      },
+      ui = { border = "single" },
     },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      {
+        "williamboman/mason-lspconfig.nvim",
+        opts = {
+          ensure_installed = {
+            "lua_ls",
+            "pyright",
+            "ts_ls",
+            "html",
+            "cssls",
+            "jsonls",
+            "bashls",
+          },
+        },
+      },
+      "hrsh7th/cmp-nvim-lsp",
+      { "j-hui/fidget.nvim", opts = {} },
+    },
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      vim.lsp.config("*", { capabilities = capabilities })
+
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.buf.hover, { border = "single" })
+      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.buf.signature_help, { border = "single" })
+
+      vim.lsp.config("lua_ls", {
+        settings = {
+          Lua = {
+            runtime = { version = "LuaJIT" },
+            diagnostics = { globals = { "vim" } },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false,
+            },
+            telemetry = { enable = false },
+          },
+        },
+      })
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspAttach", { clear = true }),
+        callback = function(event)
+          local map = function(keys, func, desc)
+            vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+          end
+
+          map("gd", vim.lsp.buf.definition, "Goto definition")
+          map("gD", vim.lsp.buf.declaration, "Goto declaration")
+          map("gr", vim.lsp.buf.references, "Goto references")
+          map("gI", vim.lsp.buf.implementation, "Goto implementation")
+          map("gy", vim.lsp.buf.type_definition, "Goto type definition")
+          map("K", vim.lsp.buf.hover, "Hover documentation")
+          map("<C-k>", vim.lsp.buf.signature_help, "Signature help")
+          map("<leader>rn", vim.lsp.buf.rename, "Rename symbol")
+          map("<leader>ca", vim.lsp.buf.code_action, "Code action")
+          map("<leader>f", function()
+            vim.lsp.buf.format({ async = true })
+          end, "Format buffer")
+
+          local client = vim.lsp.get_client_by_id(event.data.client_id)
+          if client and client.supports_method("textDocument/documentHighlight") then
+            local hl_group = vim.api.nvim_create_augroup("UserLspHighlight", { clear = false })
+            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+              buffer = event.buf,
+              group = hl_group,
+              callback = vim.lsp.buf.document_highlight,
+            })
+            vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+              buffer = event.buf,
+              group = hl_group,
+              callback = vim.lsp.buf.clear_references,
+            })
+          end
+        end,
+      })
+
+      vim.diagnostic.config({
+        virtual_text = { prefix = "●" },
+        severity_sort = true,
+        float = { border = "single" },
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN] = "",
+            [vim.diagnostic.severity.INFO] = "",
+            [vim.diagnostic.severity.HINT] = "",
+          },
+        },
+      })
+    end,
   },
 }
